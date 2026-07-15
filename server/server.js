7153744ed156b60eabce347b410e5b3087859301
olaemmadayo <olatoye.temidayo@yahoo.com>
2026-07-15 12:35:19 +0100

    API created for payslip managemnet

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ import authRouter from "./routes/authRoutes.js";
 import employeesRouter from "./routes/employeesRoutes.js";
 import profileRouter from "./routes/profileRoute.js";
 import attendanceRouter from "./routes/attendanceRoutes.js";
+import leaveRouter from "./routes/leaveRoutes.js";
+import payslipRouter from "./routes/payslipRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -22,6 +24,8 @@ app.use("/api/auth", authRouter);
 app.use("/api/employees", employeesRouter);
 app.use("/api/profile", profileRouter);
 app.use("/api/attendance", attendanceRouter);
+app.use("/api/leave", leaveRouter);
+app.use("/api/payslip", payslipRouter);
 
 await connectDB();
 app.listen(PORT, () => console.log(`Server runing on Port ${PORT}`));
